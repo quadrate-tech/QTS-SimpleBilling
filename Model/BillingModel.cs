37cd7264df7360870431d62e7badf92b79e19cc9
Quadrate Tech Solutions <info@quadrate.lk>
2021-02-06 20:40:22 +0530

    commit

---
+++ Model/BillingModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QTS_SimpleBilling.Model
+{
+    internal class BillingModel : DbContext
+    {
+
+    }
+}
