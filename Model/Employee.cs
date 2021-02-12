60b723468e078f5f142c0eb6f4b235442e1bf6bb
Quadrate Tech Solutions <info@quadrate.lk>
2021-02-12 17:35:28 +0530

    Commit Changes

---
+++ Model/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTS_SimpleBilling.Model
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [MaxLength(60)]
+        public string EmployeeName { get; set; }
+        [MaxLength(25)]
+        public string Contact { get; set; }
+        [MaxLength(40)]
+        public string Email { get; set; }
+        [MaxLength(240)]
+        public string Address { get; set; }
+        public int Status { get; set; }
+        [MaxLength(10)]
+        public string EmployeeCode { get; set; }
+    }
+}
