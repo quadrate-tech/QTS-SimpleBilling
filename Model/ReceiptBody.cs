b11efb597510ddc6fca1adb6598fdbb48e764f3e
Aravinda <aravinda73@gmail.com>
2021-02-13 01:20:56 +0530

    Another Receipt Body

---
+++ Model/ReceiptBody.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTS_SimpleBilling.Model
+{
+    class ReceiptBody
+    {
+        public int ReceiptNo { get; set; }
+        
+        public string ProductCode { get; set; }
+        
+        public string UnitPrice { get; set; }
+        
+        public string Quantity { get; set; }
+        
+        public string Discount { get; set; }
+        
+        public string SubTotal { get; set; }
+        
+        public string NetTotal { get; set; }
+        
+    }
+}
