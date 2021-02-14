5d96d587dcf0040841fe083d3b02a88a56d8aec4
Atheek <Atheek@DESKTOP-S4QLIS6>
2021-02-14 19:55:45 +0530

    initial commit

---
+++ Model/ReciptBodyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QTS.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QTS
+{
+    public class ReciptBodyDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-S4QLIS6\SQLEXPRESS_16_AT; database=Qts; Integrated Security=True");
+        }
+
+        public DbSet<ReceiptModel> receiptModels { get; set; }
+
+    }
+}
---
+++ Repository/IRepository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QTS.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Remove(T entity);
+        void Remove(int id);
+        T GetRecipt(int id);
+        IEnumerable<T> Recipts();
+
+    }
+}
