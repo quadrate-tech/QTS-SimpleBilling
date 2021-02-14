25d70c33d4516e2a780171c9b578241ff4d4335c
Mohamed Rifan <66748945+rifan1013@users.noreply.github.com>
2021-02-14 21:17:03 +0530

    Add files via upload
    #3

---
+++ QTSSimple_Billing/QTSSimple_Billing/Interface/CRUD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTSSimple_Billing.Interface
+{
+    internal interface CRUD<T>
+    {
+        int Create(T t);
+        int Update(T t);
+        int Delete(T t);
+        List<T> View();
+        List<T> Search(string t);
+    }
+}
