850a3222e19d4358d6bbf9909904802f053c3500
Atheek <55016532+atheekalm@users.noreply.github.com>
2021-02-15 07:15:04 +0530

    Add files via upload

---
+++ Repository/IRepository/IRicipt.cs
@@ -0,0 +1,14 @@
+﻿using QTS.IRepository;
+using QTS.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QTS.IRepository
+{
+    public interface IRicipt : IRepository<ReceiptModel>
+    {
+        void Update(ReceiptModel receiptModel);
+
+    }
+}
