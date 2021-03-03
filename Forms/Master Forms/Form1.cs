d2403fad38c0c13eb852d85190154f7ea4626e37
Quadrate Tech Solutions <info@quadrate.lk>
2021-03-03 20:29:29 +0530

    sssssssssssssqw

--- Forms/Master Forms/Form1.cs
+++ Forms/Master Forms/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Inv
 {
-    public partial class Form1 : Form
+    public partial class Generate_Invoice : Form
     {
-        public Form1()
+        public Generate_Invoice()
         {
             InitializeComponent();
         }
--- Model/BillingContext.cs
+++ Model/BillingContext.cs
@@ -12,13 +12,18 @@ namespace QTS_SimpleBilling.Model
             optionBuilder.UseSqlServer(@"Data Source=(LocalDb)\MSSQLLocalDB; Initial Catalog=QTS-SimpleBilling;Integrated Security=True");
         }
         public DbSet<Employee> Employees { get; set; }
+
         public DbSet<Item> Items { get; set; }
+
         public DbSet<Category> Categories { get; set; }
+
         public DbSet<Shelf> Shelves { get; set; }
+
         public DbSet<Unit> Units { get; set; }
+
         public DbSet<SellingPrice> SellingPrices { get; set; }
         public DbSet<Cost> Costs { get; set; }
-        public DbSet<Customer> Customers { get; set; }
+
 
     }
 }
